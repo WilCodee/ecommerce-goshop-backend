0821cc01f430e2084350754cfd7a5b4233e0eacd
@Wil <emma.wilinston@gmail.com>
2021-09-18 18:41:26 -0500

    arreglo query usuarios

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,27 +1,37 @@
 const Query = {
   user: async (_, { _id }, { UserDB }) => {
-    if (!_id) return await UserDB.find({})
-    const data = await UserDB.findById(_id)
-    if (!data) throw new Error('Usuario no existe')
-    return data
+    try {
+      if (!_id) return await UserDB.find({})
+      if (_id) return await UserDB.find({ _id })
+    } catch (error) {
+      throw new Error('Algo salio mal')
+    }
   },
   admin: async (_, { _id }, { AdminDB }) => {
-    const data = await AdminDB.findById(_id)
-    if (!data) throw new Error('Admin no existe')
-    return data
+    try {
+      const data = await AdminDB.findById(_id)
+      return data
+    } catch (error) {
+      throw new Error('Algo salio mal')
+    }
   },
   thirdUser: async (_, { _id }, { UserThirdServices }) => {
-    if (!_id) return await UserThirdServices.find({})
-    const data = await UserThirdServices.findById(_id)
-    if (!data) throw new Error('Usuario no existe')
-    return data
+    try {
+      if (!_id) return await UserThirdServices.find({})
+      return await UserThirdServices.find({ _id })
+    } catch (error) {
+      throw new Error('Algo salio mal')
+    }
   },
 
   product: async (_, { _id, typeProduct, limit }, { Products }) => {
-    if (_id) return await Products.find({ _id })
-    if (typeProduct) return await Products.find({ typeProduct }).limit(limit)
-    const result = Products.find({}).limit(limit)
-    return result
+    try {
+      if (_id) return await Products.find({ _id })
+      if (typeProduct) return await Products.find({ typeProduct }).limit(limit)
+      return await Products.find({}).limit(limit)
+    } catch (error) {
+      throw new Error('Algo salio mal')
+    }
   },
 }
 
